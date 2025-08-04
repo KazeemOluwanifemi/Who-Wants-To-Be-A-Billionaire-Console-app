c5a7e92380c74899e5ba263f0b1f5b71ac0df851
KazeemOluwanifemi <bethkaz934@gmail.com>
2025-08-04 15:15:01 +0100

    refactored question handling functionality

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 import java.util.*;
 //package UserStats;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
         boolean running = true;
-        while(running){
+        while (running) {
             Scanner reader = new Scanner(System.in);
 //            User Statistics Inner class instantiated
             UserStats.User user = new UserStats.User();
@@ -18,25 +18,24 @@ public class Main{
 //            Validate user's input
             int playOrExitGame = 0;
 
-            if(reader.hasNextInt()){
+            if (reader.hasNextInt()) {
                 playOrExitGame = reader.nextInt();
 //                return a method's functionality based on user's input
-                if(playOrExitGame == 1){
+                if (playOrExitGame == 1) {
                     System.out.println("Alright then, let's play!");
                     String userName = user.getName();
                     startGame(userName);
-                } else{
+                } else {
                     System.out.println("Exiting game now.");
                     running = false;
                 }
-            } else{
+            } else {
                 System.out.println("Invalid input, try again!");
             }
-            handleQuestions();
         }
     }
 
-    public static void startGame(String userName){
+    public static void startGame(String userName) {
         if (userName.isEmpty()) {
             System.out.println("An error occurred while accepting your name, please try again");
         } else {
@@ -54,64 +53,7 @@ public class Main{
                     "if you miss the next set of questions but answer that question correctly");
             System.out.println("6. There is a designated safety net for every 5 questions answered correctly");
             System.out.println("============================================================================");
-            System.out.println("Alright, that's it! Its time to earn cash " + userName + " , are you ready?");
+            System.out.println("Alright, that's it! Its time to earn cash " + userName + ", are you ready?");
         }
-    }
-
-    public static void handleQuestions(){
-        Scanner reader = new Scanner(System.in);
-
-        QuestionsHandling questionFile = new QuestionsHandling();
-//            Initialize arrayLists to store returned elements from the
-//            methods in the fileHandling class
-        ArrayList<String> questions = new ArrayList<>();
-        questions = questionFile.getQuestion("trivia_quiz.csv");
-        ArrayList<String> firstOptions = new ArrayList<>();
-        firstOptions = questionFile.firstOption();
-        ArrayList<String> secondOptions = new ArrayList<>();
-        secondOptions = questionFile.secondOption();
-        ArrayList<String> thirdOptions = new ArrayList<>();
-        thirdOptions = questionFile.thirdOption();
-        ArrayList<String> fourthOptions = new ArrayList<>();
-        fourthOptions = questionFile.fourthOption();
-        ArrayList<String> correctOptions = new ArrayList<>();
-        correctOptions = questionFile.correctOption();
-
-        String currentQuestion = "";
-        String currentFirstOpt = "";
-        String currentSecondOpt = "";
-        String currentThirdOpt = "";
-        String currentFourthOpt = "";
-        String currentAnswer = "";
-        String userAnswer = "";
-
-        for(int i = 0; i < questions.size(); i++){
-            currentQuestion = questions.get(i);
-            currentFirstOpt = firstOptions.get(i);
-            currentSecondOpt = secondOptions.get(i);
-            currentThirdOpt = thirdOptions.get(i);
-            currentFourthOpt = fourthOptions.get(i);
-            currentAnswer =  correctOptions.get(i);
-
-            System.out.println("Here's your first question: ");
-
-            System.out.println(currentQuestion);
-            System.out.println("A. " + currentFirstOpt);
-            System.out.println("B. " + currentSecondOpt);
-            System.out.println("C. " + currentThirdOpt);
-            System.out.println("D. " + currentFourthOpt);
-
-            System.out.println("Enter your answer[choose an option]: ");
-            userAnswer = reader.nextLine();
-
-            if(userAnswer.equals(currentAnswer)){
-                System.out.println("Correct!");
-                System.out.println("This question earned you #10,000");
-            } else{
-                System.out.println("Wrong Answer");
-            }
-
-        }
-
     }
 }
--- src/QuestionsHandling.java
+++ src/QuestionsHandling.java
@@ -3,121 +3,44 @@ import java.util.*;
 import java.io.*;
 
 public class QuestionsHandling {
-//    Scanner reader = new Scanner(System.in);
+    Scanner reader = new Scanner(System.in);
+//    one question class serving as a template for each object question
+    class Question{
+        String question;
+        String optionA;
+        String optionB;
+        String optionC;
+        String optionD;
+        String correctOption;
+    }
 
-    String question;
-    String optionA;
-    String optionB;
-    String optionC;
-    String optionD;
-    String correctOption;
+//    method to implement loading the file contents functionality
+    ArrayList<Question> getQuestionDet(String filename) {
+//        list containing different question objects
+        ArrayList<Question> questions = new ArrayList<>();
 
-    ArrayList<String> getQuestion(String filename){
-//        create a new list
-        ArrayList<String> questions = new ArrayList<>();
-//        validate presence of a string in the file to handle a possible exception
-        try (Scanner reader = new Scanner(Paths.get(filename))){
+        try(Scanner reader = new Scanner(Paths.get(filename))){
             while(reader.hasNextLine()){
                 String line = reader.nextLine();
 
                 String[] pieces = line.split(",");
 
-                question = pieces[0];
-                questions.add(question);
+                if(pieces.length < 6){
+                    System.out.println("There is a faulty line in the question file");
+                } else {
+                    Question theQuestion = new Question();
+                    theQuestion.question = pieces[0].trim();
+                    theQuestion.optionA = pieces[1].trim();
+                    theQuestion.optionB = pieces[2].trim();
+                    theQuestion.optionC = pieces[3].trim();
+                    theQuestion.optionD = pieces[4].trim();
+                    theQuestion.correctOption = pieces[5].trim();
+                    questions.add(theQuestion);
+                }
             }
         } catch(IOException e){
             System.out.println("Error while handling file: " + e);
         }
         return questions;
-    }
-
-    ArrayList<String> firstOption(){
-//        create a new list
-        ArrayList<String> firstOptions = new ArrayList<>();
-//        validate presence of text in file and handle possible exceptions using a try catch stuff
-        try(Scanner reader = new Scanner(Paths.get("trivia_quiz.csv"))){
-            while(reader.hasNextLine()){
-                String line = reader.nextLine();
-
-                String[] pieces = line.split(",");
-
-                optionA = pieces[1];
-                firstOptions.add(optionA);
-            }
-        } catch(IOException e){
-            System.out.println("Error while handling file: " + e);
-        }
-        return firstOptions;
-    }
-
-    ArrayList<String> secondOption(){
-        ArrayList<String> secondOptions = new ArrayList<>();
-        try(Scanner reader = new Scanner(Paths.get("trivia_quiz.csv"))){
-            while(reader.hasNextLine()){
-                String line = reader.nextLine();
-
-                String[] pieces = line.split(",");
-
-                optionB = pieces[2];
-                secondOptions.add(optionB);
-            }
-        } catch(IOException e){
-            System.out.println("Error while handling file: " + e);
-        }
-        return secondOptions;
-    }
-
-    ArrayList<String> thirdOption(){
-        ArrayList<String> thirdOptions = new ArrayList<>();
-
-        try(Scanner reader = new Scanner(Paths.get("trivia_quiz.csv"))){
-            while(reader.hasNextLine()){
-                String line = reader.nextLine();
-
-                String[] pieces = line.split(",");
-
-                optionC = pieces[3];
-                thirdOptions.add(optionC);
-            }
-        } catch(IOException e){
-            System.out.println("Error while handling file: " + e);
-        }
-        return thirdOptions;
-    }
-
-    ArrayList<String> fourthOption() {
-        ArrayList<String> fourthOptions = new ArrayList<>();
-
-        try(Scanner reader = new Scanner(Paths.get("trivia_quiz.csv"))){
-            while(reader.hasNextLine()){
-                String line = reader.nextLine();
-
-                String[] pieces = line.split(",");
-
-                optionD = pieces[4];
-                fourthOptions.add(optionD);
-            }
-        } catch(IOException e){
-            System.out.println("Error while handling file: " + e);
-        }
-        return fourthOptions;
-    }
-
-    ArrayList<String> correctOption(){
-        ArrayList<String> correctOptions = new ArrayList<>();
-
-        try(Scanner reader = new Scanner(Paths.get("trivia_quiz.csv"))){
-            while(reader.hasNextLine()){
-                String line = reader.nextLine();
-
-                String[] pieces = line.split(",");
-
-                correctOption = pieces[5];
-                correctOptions.add(correctOption);
-            }
-        } catch(IOException e){
-            System.out.println("Error while handling file: " + e);
-        }
-        return correctOptions;
     }
 }
